213c94c52608322dfb84fe7307f74984b9b69bb8
daibin <daibin.com>
2020-05-30 19:49:27 +0800

    初版提交

---
+++ src/main/java/com/luckydb/LuckydbApplication.java
@@ -0,0 +1,13 @@
+package com.luckydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckydbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckydbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luckydb/luckydb/LuckydbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckydb.luckydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckydbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
